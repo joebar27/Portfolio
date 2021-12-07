02ef113f4828965b098aed7b10095dafdb02a15a
Anger romain <anger.romain@gmail.com>
2021-12-07 15:08:57 +0100

    test liens gh-pages

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className="bgColor-3 color-2">
             <p id="footer" className="d-flex col-10 justify-content-center align-items-center my-0 ps-md-4">
-                Ce portfolio a été conçu et designer par :<Link to="/portfolio/contact" className="ms-1"> ANGER Romain</Link>
+                Ce portfolio a été conçu et designer par :<Link to="/Portfolio/contact" className="ms-1"> ANGER Romain</Link>
             </p>
         </footer>
     );
