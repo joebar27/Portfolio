e761a7975ddbc8e0d12274c0ee40c66e1005a745
Anger romain <anger.romain@gmail.com>
2021-12-07 14:42:02 +0100

    test liens gh-pages

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
         <div>
             <main className="bgColor-4">
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/a-propos" element={<Aboutme />} />
-                    <Route path="/contact" element={<Contact />} />
+                    <Route path="portfolio/" element={<Home />} />
+                    <Route path="portfolio/a-propos" element={<Aboutme />} />
+                    <Route path="portfolio/contact" element={<Contact />} />
                 </Routes>
             </main>
             <Footer />
