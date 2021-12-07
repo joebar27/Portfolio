8d7e89f5657cd4a48a3d8ea7f4057ddefa77985d
Anger romain <anger.romain@gmail.com>
2021-12-07 15:04:05 +0100

    test liens gh-pages

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,13 +5,13 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
     return (
         <nav className="bgColor-3 d-flex justify-content-around align-items-center col-12">
-            <NavLink to="/" className="bgColor-4 color-2 px-2 mx-4 mx-lg-5">
+            <NavLink to="/portfolio/" className="bgColor-4 color-2 px-2 mx-4 mx-lg-5">
                 Portfolio
             </NavLink>
-            <NavLink to="/a-propos" className="bgColor-4 color-2 px-2 mx-4 mx-lg-5">
+            <NavLink to="/portfolio/a-propos" className="bgColor-4 color-2 px-2 mx-4 mx-lg-5">
                 À-propos
             </NavLink>
-            <NavLink to="/contact" className="bgColor-4 color-2 px-2 mx-4 mx-lg-5">
+            <NavLink to="/portfolio/contact" className="bgColor-4 color-2 px-2 mx-4 mx-lg-5">
                 Contact
             </NavLink>
         </nav>
